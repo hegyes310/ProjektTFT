f0c96031b45ac83ce4666776aed41909274159dc
hegyes310 <bence.hegyes@gmail.com>
2025-01-10 16:20:17 +0100

    va

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 def print_hi(name):
-    asd = 23
-    nemtoim = "asd"
-    ti = ""
     print(f'Hi, {name}')
-    ti = asdd(ti)
-    return asd, nemtoim
-
-
-def asdd(dwdwd):
-    deded = dwdwd+ ""
-    return deded
-
 
 if __name__ == '__main__':
     print_hi('PyCharm')
