4ecd3920ac16d6d6a4769fa820bc36a8999717e9
hegyes310 <bence.hegyes@gmail.com>
2025-01-10 14:25:06 +0100

    gyak

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
     asd = 23
     nemtoim = "asd"
+    ti = ""
+    print(f'Hi, {name}')
+    ti = asdd(ti)
+    return asd, nemtoim
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def asdd(dwdwd):
+    deded = dwdwd+ ""
+    return deded
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
