0ba072ba7a991a2fa5dd236d0cb42d164ce9fd2b
hegyes310 <bence.hegyes@gmail.com>
2025-01-10 16:21:38 +0100

    vadddddd

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def print_hi(name):
-    print(f'Hi, {siaaaa(name)}')
+    asd = 23
+    nemtoim = "asd"
+    ti = ""
+    print(f'Hi, {name}')
+    ti = asdd(ti)
+    return asd, nemtoim
+
+
+def asdd(dwdwd):
+    deded = dwdwd+ ""
+    return deded
+
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-def siaaaa(valami):
-    return valami
