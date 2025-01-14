78141baeaab7741b590aeccb7acb15988bf0d032
hegyes310 <bence.hegyes@gmail.com>
2025-01-14 21:33:55 +0100

    did something else

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-def print_hi(name):
-    asd = 23
-    nemtoim = "asd"
-    ti = ""
-    print(f'Hi, {name}')
-    ti = asdd(ti)
-    return asd, nemtoim
+def kalkulator():
+    print("Üdvözöllek a kalkulátorban!")
+    print("Választhatsz a következő műveletek közül:")
+    print("1. Összeadás (+)")
+    print("2. Kivonás (-)")
+    print("3. Szorzás (*)")
+    print("4. Osztás (/)")
 
+    while True:
+        # Felhasználó által választott művelet
+        muvelet = input("\nAdd meg a művelet számát (1/2/3/4) vagy 'q' a kilépéshez: ")
 
-def asdd(dwdwd):
-    deded = dwdwd+ ""
-    return deded
+        if muvelet.lower() == 'q':
+            print("Köszönöm, hogy használtad a kalkulátort! Viszlát!")
+            break
+
+        if muvelet not in ['1', '2', '3', '4']:
+            print("Érvénytelen választás. Próbáld újra.")
+            continue
+
+        try:
+            # Számok bekérése
+            szam1 = float(input("Add meg az első számot: "))
+            szam2 = float(input("Add meg a második számot: "))
+
+            # Művelet végrehajtása
+            if muvelet == '1':
+                eredmeny = szam1 + szam2
+                print(f"Eredmény: {szam1} + {szam2} = {eredmeny}")
+            elif muvelet == '2':
+                eredmeny = szam1 - szam2
+                print(f"Eredmény: {szam1} - {szam2} = {eredmeny}")
+            elif muvelet == '3':
+                eredmeny = szam1 * szam2
+                print(f"Eredmény: {szam1} * {szam2} = {eredmeny}")
+            elif muvelet == '4':
+                if szam2 == 0:
+                    print("Hiba: Nullával nem lehet osztani.")
+                else:
+                    eredmeny = szam1 / szam2
+                    print(f"Eredmény: {szam1} / {szam2} = {eredmeny}")
+        except ValueError:
+            print("Hiba: Kérlek számot adj meg!")
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    kalkulator()
