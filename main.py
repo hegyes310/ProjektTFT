8c0f63a63a2070e28b76efd5ea17de9206bfa0cb
hegyes310 <bence.hegyes@gmail.com>
2025-01-16 15:46:29 +0100

    added 1 function

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def print_hi(name):
-    print(f'Hi, {name}')
+    print(f'Hi, {name}, {return_string()}')
+
+
+def return_string():
+    return 'Hello'
+
 
 if __name__ == '__main__':
     print_hi('PyCharm')
