ae8ef51e8b5f9099c5f3196b7dee89fb1dc71ac8
hegyes310 <bence.hegyes@gmail.com>
2025-02-17 13:16:46 +0100

    refactor the whole code

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
-def kalkulator():
-    print("Üdvözöllek a kalkulátorban!")
-    print("Választhatsz a következő műveletek közül:")
-    print("1. Összeadás (+)")
-    print("2. Kivonás (-)")
-    print("3. Szorzás (*)")
-    print("4. Osztás (/)")
+import requests
+from langfuse.client import Langfuse
+import json
+import pandas as pd
 
-    while True:
-        # Felhasználó által választott művelet
-        muvelet = input("\nAdd meg a művelet számát (1/2/3/4) vagy 'q' a kilépéshez: ")
+def kalkulator(dataset_name: str,
+    run_id: str,
+    output_path: str):
+    langfuse_client = Langfuse(
+    )
+    dataset_run = langfuse_client.get_dataset_run(
+        dataset_name=dataset_name, dataset_run_name=run_id
+    )
 
-        if muvelet.lower() == 'q':
-            print("Köszönöm, hogy használtad a kalkulátort! Viszlát!")
-            break
+    run_items = []
+    index = 0
+    count = len(dataset_run.dataset_run_items)
+    for run_item in dataset_run.dataset_run_items:
+        run_item_dict = {
+            "id": run_item.id,
+            "dataset_item_id": run_item.dataset_item_id,
+            "trace_id": run_item.trace_id,
+            "created_at": run_item.created_at,
+            "updated_at": run_item.updated_at,
+        }
+        proba = langfuse_client.get_dataset_item(run_item.dataset_item_id)
+        trace = langfuse_client.fetch_trace(id=run_item.trace_id)
+        run_item_dict["trace_name"] = trace.data.name
+        # Example - Get fields from json input
+        run_item_dict["input"] = trace.data.input
+        run_item_dict["output"] = trace.data.output
+        run_item_dict["expected_output"] = proba.expected_output
+        run_item_dict["comment"] = trace.data.scores[0].comment
+        run_item_dict["score"] = trace.data.scores[0].value
 
-        if muvelet not in ['1', '2', '3', '4']:
-            print("Érvénytelen választás. Próbáld újra.")
-            continue
+        # Example - Get fields from json output
+        run_items.append(run_item_dict)
+        print(f"index: {index}/{count}")
+        index += 1
 
-        try:
-            # Számok bekérése
-            szam1 = float(input("Add meg az első számot: "))
-            szam2 = float(input("Add meg a második számot: "))
-
-            # Művelet végrehajtása
-            if muvelet == '1':
-                eredmeny = szam1 + szam2
-                print(f"Eredmény: {szam1} + {szam2} = {eredmeny}")
-            elif muvelet == '2':
-                eredmeny = szam1 - szam2
-                print(f"Eredmény: {szam1} - {szam2} = {eredmeny}")
-            elif muvelet == '3':
-                eredmeny = szam1 * szam2
-                print(f"Eredmény: {szam1} * {szam2} = {eredmeny}")
-            elif muvelet == '4':
-                if szam2 == 0:
-                    print("Hiba: Nullával nem lehet osztani.")
-                else:
-                    eredmeny = szam1 / szam2
-                    print(f"Eredmény: {szam1} / {szam2} = {eredmeny}")
-        except ValueError:
-            print("Hiba: Kérlek számot adj meg!")
+    df = pd.DataFrame(run_items)
+    df.to_csv(output_path, index=False, encoding="utf-8", sep=";")
 
 
 if __name__ == '__main__':
-    kalkulator()
+    dataset_name = "Talkabot"
+    run_id = "Evaluation_Correctness_sas_v1"
+    output_path = f"{run_id}.csv"
+    kalkulator(dataset_name, run_id, output_path)
